a569dfa274db93b8e9ea9dbb5ded895e7a7916d3
VD-17 <vidhimaisuria1709@gmail.com>
2026-06-20 11:38:58 +0200

    minor gallery change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\MenuItem;
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
